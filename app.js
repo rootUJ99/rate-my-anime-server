88781e6b06f664209a158893ba383d6d1d97bdf0
Ujwal <ujwal@symboinsurance.com>
2020-02-25 17:07:46 +0530

    removed commented code

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
-// import fs from 'fs';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import indexRouter from './routes/index';
@@ -26,30 +25,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// const getCircularReplacer = () => {
-//   const seen = new WeakSet();
-//   return (key, value) => {
-//     if (typeof value === "object" && value !== null) {
-//       if (seen.has(value)) {
-//         return;
-//       }
-//       seen.add(value);
-//     }
-//     return value;
-//   };
-// };
-// app.get('/api/animelist', (req,res)=>{
-//   console.log('req', req);
-//   fs.writeFile('req.txt', JSON.stringify(req, getCircularReplacer(), 4), 'utf8', err=>{console.log(err,'err')})
-//   fs.writeFile('res.txt', JSON.stringify(res, getCircularReplacer(), 4), 'utf8', err=>{console.log(err,'err')})
-//   console.log('res', res);
-// })
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   next();
-// });
 
 const exceptPaths = (pathArr, middleware) => {
   return (req, res, next) => {
